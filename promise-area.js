d6296ca61d57782b13adc7082d8312c8c1e475ba
Lucas Nogueira Ramos <lucasnramos@gmail.com>
2019-10-23 17:51:38 -0300

    Solved promise area

--- promise-area.js
+++ promise-area.js
@@ -6,25 +6,38 @@ function calculateArea(shape, values) {
         case "square":
             area = n1 * n1;
             break;
+        case "rectangle":
+            area = n1 * n2;
+            break;
+        case "circle":
+            area = 3.14 * (n1 * n1);
+            break;
+        case "triangle":
+            area = 0.5 * n1 * n2;
+            break;
         }
 
-        if (area <= 0) reject(-1);
+        if (area <= 0) reject([-1]);
         area = +area.toFixed(2);
         resolve(area);
     });
+
 }
 
 function getAllAreas(shapes, values_arr) {
     return new Promise((reject, resolve) => {
         const areas = []
 
-        for() {
+        for(let i = 0; i < shapes.length; i++) {
             const area = calculateArea(shapes[i], values_arr[i]);
             areas.push(area);
         }
 
         Promise.all(areas)
             .then((_areas) => resolve(_areas))
-            .catch(err => console.error(err));
+            .catch(err => {
+                console.error(err);
+                reject([-1]);
+            });
     });
 }
